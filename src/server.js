dc4133a298040fe5bcbdc2b77529aa301beaffe6
Arundhathi R Menon <43034600+arundhathi6@users.noreply.github.com>
2022-01-25 15:05:45 +0530

    again 2345

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 const app = require("./index");
 const connect = require("./configs/db");
 
-app.listen(5567, async () => {
+app.listen(2345, async () => {
   try {
     await connect();
-    console.log("listening on port 5567");
+    console.log("listening on port 2345");
   } catch (e) {
     console.log(e.message);
   }
